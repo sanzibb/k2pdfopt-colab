9e8dfb8071dfbf994248127edb87cc3777f94c20
Sanjeev Saha <1904992+sanzibb@users.noreply.github.com>
2026-07-03 21:23:05 +0530

    Update build_notebook.py

--- src/build_notebook.py
+++ src/build_notebook.py
@@ -1,1 +1,12 @@
-py
+import nbformat
+
+from milestone1 import cells
+
+nb = nbformat.v4.new_notebook()
+
+nb.cells = cells()
+
+with open("../notebook/k2pdfopt_colab.ipynb", "w", encoding="utf-8") as f:
+    nbformat.write(nb, f)
+
+print("Notebook written.")
